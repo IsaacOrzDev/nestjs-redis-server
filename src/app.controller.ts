ab73edf9a1794ed01ae1921c50e90292649d2254
Isaac <isaachkdeveloper@gmail.com>
2022-10-17 23:27:26 +0800

    listen message from redis

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern({ cmd: 'sum' })
+  accumulate(data: number[]): Promise<number> {
+    return new Promise((resolve) =>
+      resolve((data || []).reduce((a, b) => a + b)),
+    );
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
+const microserviceOptions: MicroserviceOptions = {
+  transport: Transport.REDIS,
+  options: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+  } as any,
+};
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    microserviceOptions,
+  );
+  await app.listen();
 }
 bootstrap();
